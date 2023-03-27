e71282e32c92bb52b3c4d84e1032e4cc73090a49
sofiakwok <zazakwok@gmail.com>
2023-03-27 11:49:49 -0400

    yay

--- wordfinder.py
+++ wordfinder.py
@@ -93,10 +93,6 @@ def free_space(counter):
     return free_left, free_right, free_up, free_down
 
 data = iter(open("dictionary.txt", "r"))
-words = []
-for element in data:
-    stripped = element.strip("\n")
-    words.append(stripped.split(" "))
 
 grid_size = 11
 letter_value = {'A':1 , 'B':3, 'C':3, 'D':2, 'E':1, 'F':4, 
@@ -106,7 +102,7 @@ letter_value = {'A':1 , 'B':3, 'C':3, 'D':2, 'E':1, 'F':4,
 grid_state = np.chararray((grid_size, grid_size))
 grid_state[:] = b'0'
 
-player_letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
+player_letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O']
 grid_state[5, 6] = 'C'
 grid_state[5, 7] = 'H'
 grid_state[5, 8] = 'A'
@@ -115,98 +111,104 @@ grid_state[5, 10] = 'T'
 grid_state[4, 7] = 'A'
 prettyprint(grid_state)
 
-grid_letters = []
-grid_positions = []
-for i in range(grid_size):
-    for j in range(grid_size):
-        if grid_state[i, j] != b'0':
-            grid_letters.append(grid_state[i, j])
-            grid_positions.append([i, j])
+def findBestMove(first_move=False):
+    words = []
+    for element in data:
+        stripped = element.strip("\n")
+        words.append(stripped.split(" "))
 
-#print(grid_positions)
-#print(grid_letters)
+    grid_letters = []
+    grid_positions = []
+    for i in range(grid_size):
+        for j in range(grid_size):
+            if grid_state[i, j] != b'0':
+                grid_letters.append(grid_state[i, j])
+                grid_positions.append([i, j])
 
-plays = []
-for i in range(len(grid_letters)):
-    # check how many horizonal and vertical spaces around tile are free
-    free_left, free_right, free_up, free_down = free_space(i)
-    letter = grid_letters[i].decode()
-    #print(letter)
-    col, row = grid_positions[i]
+    #print(grid_positions)
+    #print(grid_letters)
 
-    axes = np.array([free_left, free_right, free_up, free_down])
-    #print("free spaces: " + str(axes))
-    free_grid = np.array([free_left + free_right, free_up + free_down])
-    free_spaces = max(free_grid)
+    plays = []
+    for i in range(len(grid_letters)):
+        # check how many horizonal and vertical spaces around tile are free
+        free_left, free_right, free_up, free_down = free_space(i)
+        letter = grid_letters[i].decode()
+        #print(letter)
+        row, col = grid_positions[i]
 
-    #find whether free space is horizontal or vertical
-    free_axis = np.argmax(free_grid)
-    negative_direction = axes[free_axis*2]
-    positive_direction = axes[free_axis*2 + 1]
+        axes = np.array([free_left, free_right, free_up, free_down])
+        #print("free spaces: " + str(axes))
+        free_grid = np.array([free_left + free_right, free_up + free_down])
+        free_spaces = max(free_grid)
 
-    #check to make sure we're not at a corner or an intersection
-    if free_spaces > 0 and negative_direction > 0 and positive_direction > 0:
-        #iterate through list of words to find possible combinations with tiles in player hand (left/right and up/down)
+        #find whether free space is horizontal or vertical
+        free_axis = np.argmax(free_grid)
+        negative_direction = axes[free_axis*2]
+        positive_direction = axes[free_axis*2 + 1]
 
-        #check whether words in dictionary are shorter than the number of free spaces
-        #check whether words only use letters in the player's hand
-        possible_plays = []
-        for line in words:
-            word = line[0]
-            player_letters_temp = player_letters.copy()
-            player_letters_temp.append(letter)
-            if len(word) < free_spaces:
+        #check to make sure we're not at a corner or an intersection
+        if free_spaces > 0 and negative_direction > 0 and positive_direction > 0:
+            #iterate through list of words to find possible combinations with tiles in player hand (left/right and up/down)
+
+            #check whether words in dictionary are shorter than the number of free spaces
+            #check whether words only use letters in the player's hand
+            possible_plays = []
+            for line in words:
+                word = line[0]
+                player_letters_temp = player_letters.copy()
+                player_letters_temp.append(letter)
+                if len(word) < free_spaces:
+                    for l in range(len(word)):
+                        if player_letters_temp.count(word[l]) > 0:
+                            player_letters_temp.remove(word[l])
+                        else:
+                            break
+                        if l == len(word) - 1:
+                            possible_plays.append(word)
+                            break
+            #print(possible_plays)
+
+            #check whether tile location in the word will not interfere with freespace amount
+            for word in possible_plays:
+                tile_locations = []
                 for l in range(len(word)):
-                    if player_letters_temp.count(word[l]) > 0:
-                        player_letters_temp.remove(word[l])
-                    else:
-                        break
-                    if l == len(word) - 1:
-                        possible_plays.append(word)
-                        break
-        #print(possible_plays)
+                    if word[l] == letter:
+                        tile_locations.append(l)
+                for location in tile_locations:
+                    if negative_direction - (location + 1) > 0 and positive_direction - (len(word) - (location + 1)) > 0:
+                        plays.append([word, row, col, location, free_axis])
 
-        #check whether tile location in the word will not interfere with freespace amount
-        for word in possible_plays:
-            tile_locations = []
-            for l in range(len(word)):
-                if word[l] == letter:
-                    tile_locations.append(l)
-            for location in tile_locations:
-                if negative_direction - (location + 1) > 0 and positive_direction - (len(word) - (location + 1)) > 0:
-                    plays.append([word, col, row, location, free_axis])
+    #print(plays)
+    scores = []
+    #calculate number of points for each possible play
+    for possible_play in plays:
+        word = possible_play[0]
+        points = 0
+        for l in range(len(word)):
+            points += letter_value[word[l]]
+        scores.append(points)
 
-#print(plays)
-scores = []
-#calculate number of points for each possible play
-for possible_play in plays:
-    word = possible_play[0]
-    points = 0
-    for l in range(len(word)):
-        points += letter_value[word[l]]
-    scores.append(points)
+    #find highest-scoring play
+    best_play = plays[np.argmax(scores)]
+    word = best_play[0]
+    tile_position = [best_play[1], best_play[2]]
 
-#find highest-scoring play
-best_play = plays[np.argmax(scores)]
-word = best_play[0]
-tile_position = [best_play[1], best_play[2]]
+    print("best play: " + str(word))
+    print("tile position: " + str(tile_position))
+    print("points: " + str(scores[np.argmax(scores)]))
 
-print("best play: " + str(word))
-print("tile position: " + str(tile_position))
-print("points: " + str(scores[np.argmax(scores)]))
-
-tile_in_word = best_play[3]
-#print(tile_in_word)
-#0 - horizontal, 1 - vertical
-axis_orientation = best_play[4]
-#print("axis orientation: " + str(axis_orientation))
-for i in range(len(word)):
-    if i != tile_in_word:
-        if i < tile_in_word:
-            grid_state[tile_position[0] - (axis_orientation)*(tile_in_word - i), tile_position[1] - (1 - axis_orientation)*(tile_in_word - i)] = word[i]
-        elif i > tile_in_word:
-            grid_state[tile_position[0] + (axis_orientation)*(i - tile_in_word), tile_position[1] + (1 - axis_orientation)*(i - tile_in_word)] = word[i]
-prettyprint(grid_state)
+    tile_in_word = best_play[3]
+    #print(tile_in_word)
+    #0 - horizontal, 1 - vertical
+    axis_orientation = best_play[4]
+    #print("axis orientation: " + str(axis_orientation))
+    for i in range(len(word)):
+        if i != tile_in_word:
+            if i < tile_in_word:
+                grid_state[tile_position[0] - (axis_orientation)*(tile_in_word - i), tile_position[1] - (1 - axis_orientation)*(tile_in_word - i)] = word[i]
+            elif i > tile_in_word:
+                grid_state[tile_position[0] + (axis_orientation)*(i - tile_in_word), tile_position[1] + (1 - axis_orientation)*(i - tile_in_word)] = word[i]
+    prettyprint(grid_state)
 
 
 
